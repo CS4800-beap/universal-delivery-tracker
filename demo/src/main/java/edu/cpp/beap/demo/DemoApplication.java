664758803b5d51e2b539a0a0e54151b6c3bbfba7
philripd <phillipanddang@gmail.com>
2022-02-23 13:32:06 -0800

    Added login URL

--- demo/src/main/java/edu/cpp/beap/demo/DemoApplication.java
+++ demo/src/main/java/edu/cpp/beap/demo/DemoApplication.java
@@ -25,9 +25,9 @@ public class DemoApplication {
 	}
 	
 	
-//	@GetMapping("/hello")
-//	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-//	return String.format("Hello %s!", name);
-//	}
+	@GetMapping("/login")
+	public String login() {
+	return String.format("This will be our login page");
+	}
 
 }
