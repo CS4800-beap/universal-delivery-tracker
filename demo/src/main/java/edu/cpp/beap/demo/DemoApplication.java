424020c52b2ad4ca3430fff02c058336f807496d
Daniel Phung <danielphung01@gmail.com>
2022-02-22 23:53:12 -0800

    Added simple URL to DemoApplication.java

--- demo/src/main/java/edu/cpp/beap/demo/DemoApplication.java
+++ demo/src/main/java/edu/cpp/beap/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package edu.cpp.beap.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,6 +17,11 @@ public class DemoApplication {
 	@GetMapping("/hello")
 	public String hello() {
 	return "Hello BEAP";
+	}
+	
+	@GetMapping("/greet")
+	public String greet(@RequestParam(value = "name", defaultValue = "User") String name) {
+	return String.format("Greetings %s!", name);
 	}
 	
 	
