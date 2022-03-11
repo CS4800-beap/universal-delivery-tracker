c60b6120f2967e84d63b2e103be00ca8e2b5a0cf
philripd <phillipanddang@gmail.com>
2022-03-10 23:30:14 -0800

    Add unit test for negative numbers

--- src/test/java/edu/cpp/beap/universaldeliverytracker/calculator/CalculatorTest.java
+++ src/test/java/edu/cpp/beap/universaldeliverytracker/calculator/CalculatorTest.java
@@ -3,8 +3,6 @@ package edu.cpp.beap.universaldeliverytracker.calculator;
 import org.junit.*;
 import org.junit.jupiter.api.Test;
 
-import edu.cpp.beap.universaldeliverytracker.calculator.Calculator;
-
 public class CalculatorTest {
 	
 	@Test
@@ -14,4 +12,10 @@ public class CalculatorTest {
 		Assert.assertEquals(30, res);
 	}
 
+	@Test
+	public void testAdd2() {
+		Calculator c = new Calculator();
+		int res = c.add(-10,-20);
+		Assert.assertEquals(-30, res);
+	}
 }
