6e106eebf1975a973b22f6d4526a94eb123bf138
philripd <phillipanddang@gmail.com>
2022-03-13 17:18:54 -0700

    Connected and packaged React and Spring together; Make React frontend show in base URL; Added test API

--- trackerfrontend/src/App.js
+++ trackerfrontend/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  // 
+  useEffect(() => {
+    fetch("/api/currenttime")
+      .then((response) => response.text())
+      .then((message) => {
+        setMessage(message);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">{message}</h1>
       </header>
     </div>
   );
