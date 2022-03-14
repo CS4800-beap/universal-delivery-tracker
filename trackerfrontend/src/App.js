5279199026addee902328e7574b44036aa47502f
danielphung01 <danielphung01@gmail.com>
2022-03-14 13:05:34 -0700

    Added simple text input box - Removed default react app elements

--- trackerfrontend/src/App.js
+++ trackerfrontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -12,11 +11,16 @@ function App() {
       .then((message) => {
         setMessage(message);
       });
-  }, []);
+  }, []); 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <form>
+          <div>
+            <label name="trackingNumber">Tracking Number: </label>
+            <input className="trackingNumberInput" type="textbox" size="30" style={{ 'font-size': "20px"}} name="trackingNumber"></input>
+          </div>
+        </form>
         <h1 className="App-title">{message}</h1>
       </header>
     </div>
