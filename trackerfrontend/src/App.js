dd99fd3068e4805676b3b1890ddb1b1106379ee5
philripd <phillipanddang@gmail.com>
2022-03-15 00:51:19 -0700

    Integrated DHL Shipment Tracking API

--- trackerfrontend/src/App.js
+++ trackerfrontend/src/App.js
@@ -1,14 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [trackingNumber, setTrackingNumber] = useState("");
+  const [trackingNumber, setInput] = useState("");
+  const [DHLResponse, setDHLResponse] = useState("");
   
-  function handleGet(event) {
+  function getDHLTracking(event) {
     event.preventDefault()
-    setTrackingNumber(input)
-    console.log("Tracking Number: ", trackingNumber)
+    console.log("DHL Tracking Number: ", trackingNumber)
+
+    const options = {method: 'GET', headers: {'DHL-API-Key': process.env.REACT_APP_DHL_API_KEY}};
+
+    fetch(`https://api-eu.dhl.com/track/shipments?trackingNumber=${trackingNumber}`, options)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        setDHLResponse(JSON.stringify(response))
+      })
+      .catch(err => console.error(err));
   }
 
   return (
@@ -16,20 +25,18 @@ function App() {
       <header className="App-header">
         <div className="Input-div">
           <label className="Tracking-number">Tracking Number:</label>
-          <input className="Tracking-number-input" type="text" value={input} onInput={(e) => setInput(e.target.value)}></input>
-          <button className="Get-button"  onClick={handleGet}>Get Tracking Status</button>
+          <input className="Tracking-number-input" type="text" value={trackingNumber} onInput={(e) => setInput(e.target.value)}></input>
+          <button className="Get-button"  onClick={getDHLTracking}>Get Tracking Status</button>
         </div>
         
         <div className="Output-div">
           {/*Output data goes here*/}
-          <p className="Output-1">Output: {trackingNumber}</p>
+          <p className="Output-1">Output:</p>
+          <p className="Output-1">{DHLResponse}</p>
         </div>
       </header>
     </div>
   );
-
-  
-}
-
+};
 
 export default App;
