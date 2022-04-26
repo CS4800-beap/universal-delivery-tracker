5ab48e03f295a7e1236cc22b1366cbc3dc6819c5
philripd <phillipanddang@gmail.com>
2022-04-25 20:59:51 -0700

    Add dropdown menu for courier selection - Removed tracking number parser - Refactored code - Updated node modules

--- trackerfrontend/src/components/Home.js
+++ trackerfrontend/src/components/Home.js
@@ -5,12 +5,22 @@ import "../App.css";
 function Home() {
     
     const [trackingNumber, setTrackingNumber] = useState();
+    const couriers = [
+        {label: 'Select a courier', value: 'Select a courier'},
+        {label: 'DHL', value: 'DHL'},
+        {label: 'FedEx', value: 'FedEx'},
+      ];
+    const handleDropdownChange = (event) => {
+        setCourier(event.target.value);
+      };
+    const [courier, setCourier] = useState();
 
-    const [courier, setCourier] = useState("Please enter a tracking number.");
     const [trackingOrigin, setTrackingOrigin] = useState();
     const [trackingDestination, setTrackingDestination] = useState();
     
-    const [trackingDataValid, setTrackingDataValid] = useState(false);
+    const [trackingInputError, setTrackingInputError] = useState();
+    const [trackingResponseValid, setTrackingResponseValid] = useState(false);
+    const [trackingResponseDataValid, setTrackingResponseDataValid] = useState(false);
     const [trackingEvents, setTrackingEvents] = useState();
     const [trackingRawResponse, setTrackingRawResponse] = useState();
     
@@ -18,72 +28,64 @@ function Home() {
     // Get tracking information
     function getTracking(event) {
         event.preventDefault();
-
-        /*
-        Identify courier based on tracking number format
-
-        DHL:
-        - Source: https://www.trackingmore.com/tracking-status-detail-en-237.html
-        - 9-10 digits: 7777777770
-        - JD + 18 digits: JD000000000000000000
-        - JJD + 16 digits: JJD0000000000000000
-        */
-        if (((trackingNumber.length === 9 || trackingNumber.length === 10) && /^[0-9]+$/.test(trackingNumber)) ||
-            (trackingNumber.substring(0, 2) === "JD" && trackingNumber.substring(2).length === 18 && /^[0-9]+$/.test(trackingNumber.substring(2))) ||
-            (trackingNumber.substring(0, 3) === "JJD" && trackingNumber.substring(3).length === 16 && /^[0-9]+$/.test(trackingNumber.substring(3)))) {
-                setCourier("DHL");
+        setTrackingResponseValid(false);
+        
+        // Check if the user entered a valid tracking number
+        if (trackingNumber.length > 0) {
+            // DHL selected
+            if (courier === "DHL") {
 
                 // Send HTTP GET request to DHL's track/shipments API endpoint with the user's tracking number
                 axios.get("https://api-eu.dhl.com/track/shipments?trackingNumber=" + trackingNumber, {
                     headers: {"DHL-API-Key": process.env.REACT_APP_DHL_API_KEY}
                 })
                 .then((response) => {
-                    setTrackingDataValid(true);
+                    setTrackingResponseValid(true);
+                    setTrackingResponseDataValid(true);
                     
                     // Check if response.data.shipments[0].events has all required elements:
                     // timestamp, location.address.addressLocality, description
                     const events = response.data.shipments[0].events;
                     events.forEach(function(checkedEvent) {
                         if (checkedEvent.timestamp == null || checkedEvent.location.address.addressLocality == null || checkedEvent.description == null)
-                            setTrackingDataValid(false);
+                            setTrackingResponseDataValid(false);
                     })
-
+    
                     // If the response has all required data, assign them to React variables
-                    if (trackingDataValid) {
+                    if (trackingResponseDataValid) {
                         setTrackingOrigin(response.data.shipments[0].origin.address.addressLocality);
                         setTrackingDestination(response.data.shipments[0].destination.address.addressLocality);
                         setTrackingEvents(response.data.shipments[0].events);
                     }
                     
                     setTrackingRawResponse(JSON.stringify(response, null, 4));
-
+    
                     // Validate token if it exists
                     // Save package information in database if user is logged in.
                     var token = "";
                     token = sessionStorage.getItem("token");
                     token = localStorage.getItem("token");
                 })
-                .catch(error => console.error(error));
-        /*
-        FedEx:
-        - Source: https://www.trackingmore.com/tracking-status-detail-en-236.html
-        - 10/12/15/20/22 digits: 794843185271
-        */
-        } else if ((trackingNumber.length === 10 || trackingNumber.length === 12 || trackingNumber.length === 15 ||
-                    trackingNumber.length === 20 || trackingNumber.length === 22) && /^[0-9]+$/.test(trackingNumber)) {
-            setCourier("Fedex");
+                .catch(error => {
+                    console.error(error);
+                    setTrackingInputError("Please enter a valid tracking number.");
+                });
+            // No specific courier is selected
+            } else if (courier === "Select a courier") {
+                setTrackingInputError("Please select a courier.");
+            // Unsupported courier selected
+            } else {
+                setTrackingInputError("This courier is currently not supported.");
+            }
         // No tracking number is entered
         } else if (trackingNumber.length === 0) {
-            setCourier("Please enter a tracking number.");
-        // Unsupported tracking number is entered
-        } else {
-            setCourier("The tracking number you have entered is currently not supported.");
+            setTrackingInputError("Please enter a valid tracking number.");
         }
     }
 
     return (
         <div className="App-body">
-            <div style={{ height: 50 }}></div> {/* Temporary buffer space */}
+            <div style={{height: 50}}></div> {/* Temporary buffer space */}
 
             <div className="Tracking-input-div">
                 <label className="Tracking-number">Tracking Number:</label>
@@ -92,18 +94,25 @@ function Home() {
             </div>
 
             <div className="Tracking-input-div">
-                {!(courier === "DHL" || courier === "FedEx") &&
-                    <p>{courier}</p>
+                <select value={courier} onChange={handleDropdownChange}>
+                    {couriers.map((option) => (
+                        <option value={option.value}>{option.label}</option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="Tracking-input-div">
+                {!trackingResponseValid &&
+                    <p>{trackingInputError}</p>
                 }
             </div>
             
-            {courier === "DHL" &&
+            {(courier === "DHL" && trackingResponseValid) &&
                 <div className="Tracking-output-div">
-                    <p className="Tracking-header">Courier: {courier}</p>
                     <p className="Tracking-header">Origin: {trackingOrigin}</p>
                     <p className="Tracking-header">Destination: {trackingDestination}</p>
 
-                    {trackingDataValid &&
+                    {trackingResponseDataValid &&
                         <div className="Tracking-status-table">
                         <table>
                             <thead>
