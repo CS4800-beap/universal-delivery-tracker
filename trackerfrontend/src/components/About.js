b5d3865aa1c54d040ed04870b339c2710eb0621c
Huang Yi <yihuang@cpp.edu>
2022-04-26 10:14:36 -0700

    Add external URL to github

--- trackerfrontend/src/components/About.js
+++ trackerfrontend/src/components/About.js
@@ -2,11 +2,22 @@ import React from "react";
 import "../App.css";
 
 function About() {
+
+  function gitUrl() {
+    window.location.href="https://github.com/CS4800-beap/universal-delivery-tracker"; 
+  }
+
   return (
     <div className="App-body">
         <h1>
             About Page
         </h1>
+
+        <p>Our Code</p> 
+        <button onClick={gitUrl} style={{margin: '1vh'}}>
+            GitHub
+        </button>
+
     </div>
   );
 };
