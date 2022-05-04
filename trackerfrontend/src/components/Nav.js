bc3f9c92539fc7ab200160abb14d141f5842e7ae
philripd <phillipanddang@gmail.com>
2022-05-03 23:10:05 -0700

    Add logo

--- trackerfrontend/src/components/Nav.js
+++ trackerfrontend/src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../images/delivtruck-small-white.png";
 import { Link } from "react-router-dom";
 import "../App.css";
 
@@ -6,7 +7,9 @@ function Nav() {
 
   return (
     <nav>
-        <h3><Link style={{color: "white", textDecoration: "none"}} to="/">Logo</Link></h3>
+        <h3><Link style={{color: "white", textDecoration: "none"}} to="/">
+          <img src={logo} alt="Logo"/></Link>
+        </h3>
         <ul className="Nav-items">
             <li>
               <Link className="Nav-item" to="/">Home</Link>
